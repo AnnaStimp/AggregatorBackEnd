b06839686256314046a588b7148f70f343d5722e
AnnaStimp <strashenkoanna@mail.ru>
2022-05-26 14:12:34 +0300

    f

--- model.py
+++ model.py
@@ -62,24 +62,24 @@ def get_products (cursor):
 
   return cursor.fetchall()
 
-# функция для внесения данных о товаре в базу данных
-def insert_product (cursor, name, about, url, volume, id_category):
-  cursor.execute(
-    """INSERT INTO product (name_product, about_product, picture_product, volume, id_category)
-        VALUES ('{}', '{}', '{}', '{}', {})
-        RETURNING id_product;""".format(name, about, url, volume, id_category)
-  )
+# # функция для внесения данных о товаре в базу данных
+# def insert_product (cursor, name, about, url, volume, id_category):
+#   cursor.execute(
+#     """INSERT INTO product (name_product, about_product, picture_product, volume, id_category)
+#         VALUES ('{}', '{}', '{}', '{}', {})
+#         RETURNING id_product;""".format(name, about, url, volume, id_category)
+#   )
 
-  return cursor.fetchall()
+#   return cursor.fetchall()
 
-# функция для внесения данных в прайс-лист конкретного товара в базу данных
-def insert_price_list (cursor, id_product, id_store, link_product, price):
-  cursor.execute(
-    """INSERT INTO price_list (id_product, id_store, link_product, price)
-        VALUES ({}, {}, '{}', {});""".format(id_product, id_store, link_product, price)
-  )
+# # функция для внесения данных в прайс-лист конкретного товара в базу данных
+# def insert_price_list (cursor, id_product, id_store, link_product, price):
+#   cursor.execute(
+#     """INSERT INTO price_list (id_product, id_store, link_product, price)
+#         VALUES ({}, {}, '{}', {});""".format(id_product, id_store, link_product, price)
+#   )
 
-  return cursor.statusmessage
+#   return cursor.statusmessage
 
 # функция для обновления информации о просмотре товара в базе данных
 def product_viewing (cursor, id_product):
